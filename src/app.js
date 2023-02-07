2d4cfc42e7091cd5f349d7ea046f024fd3ce41c7
Schermann Tilla Réka <schtilla@gmail.com>
2023-02-07 21:27:29 +0100

    JS kész

--- src/app.js
+++ src/app.js
@@ -7,3 +7,25 @@ Date: 2023.02.07
 Github: 
 Licenc: GNU GPL
 */
+
+let radius1=document.querySelector("#sugar1")
+let radius2=document.querySelector("#sugar2");
+let height=document.querySelector("#magassag");
+
+const calcButton = document.querySelector("#KalkGomb");
+
+const volume = document.querySelector("#terfogat");
+
+KalkGomb.addEventListener('click', ()=>{
+    kalkRadius1 = Number(radius1.value);
+    kalkRadius2 = Number(radius2.value);
+    KalkHeight = Number(height.value);
+
+    volume.value=parseFloat(calcVol(kalkRadius1, kalkRadius2, KalkHeight)).toFixed(3);
+
+})
+
+function calcVol(r1, r2, m) {
+    let terfogat = (1/3)*Math.PI*r1*r2*m;
+    return terfogat;
+}
